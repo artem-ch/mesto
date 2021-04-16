00269a322ac93492cb4901faf891a73aff90832f
Artem Chagin <artem2020wd@yandex.ru>
2021-04-17 01:32:25 +0300

    refactor: delete old imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import './index.css';
+
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
+
 import {
-  initialCards,
   settings,
   containerSelector,
   editPopupSelector,
@@ -16,50 +17,51 @@ import {
   editButton,
   addButton
 } from '../utils/constants.js';
+
 import { createCard } from '../utils/utils.js';
 
 const userInfo = new UserInfo({ profileNameSelector, profileAboutSelector });
 const editValidator = new FormValidator(settings, editPopupSelector);
 const addValidator = new FormValidator(settings, addPopupSelector);
 
-const cardList = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    const cardElement = createCard(item);
-    cardList.addItem(cardElement);
-  }
-}, containerSelector);
+// const cardList = new Section({
+//   items: initialCards,
+//   renderer: (item) => {
+//     const cardElement = createCard(item);
+//     cardList.addItem(cardElement);
+//   }
+// }, containerSelector);
 
-const editPopup = new PopupWithForm({
-  submitter: ({ name, about }) => {
-    userInfo.setUserInfo({ name, about })
+// const editPopup = new PopupWithForm({
+//   submitter: ({ name, about }) => {
+//     userInfo.setUserInfo({ name, about })
 
-    editPopup.close();
-  }
-}, editPopupSelector);
+//     editPopup.close();
+//   }
+// }, editPopupSelector);
 
-editButton.addEventListener('click', () => {
-  nameInput.value = userInfo.getUserInfo().name;
-  aboutInput.value = userInfo.getUserInfo().about;
+// editButton.addEventListener('click', () => {
+//   nameInput.value = userInfo.getUserInfo().name;
+//   aboutInput.value = userInfo.getUserInfo().about;
 
-  editValidator.resetErrors();
-  editPopup.open();
-});
+//   editValidator.resetErrors();
+//   editPopup.open();
+// });
 
-const addPopup = new PopupWithForm({
-  submitter: ({ name, link }) => {
-    const cardElement = createCard({ name, link });
-    cardList.prependItem(cardElement);
-    addPopup.close();
-  }
-}, addPopupSelector);
+// const addPopup = new PopupWithForm({
+//   submitter: ({ name, link }) => {
+//     const cardElement = createCard({ name, link });
+//     cardList.prependItem(cardElement);
+//     addPopup.close();
+//   }
+// }, addPopupSelector);
 
-addButton.addEventListener('click', () => {
-  addValidator.resetErrors();
-  addPopup.open();
-});
+// addButton.addEventListener('click', () => {
+//   addValidator.resetErrors();
+//   addPopup.open();
+// });
 
 
-cardList.renderItems();
-editValidator.enableValidation();
-addValidator.enableValidation();
+// cardList.renderItems();
+// editValidator.enableValidation();
+// addValidator.enableValidation();
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,37 +1,3 @@
-import arkhyz from '../images/mesto-russia-arkhyz.jpg';
-import chelyabinsk from '../images/mesto-russia-chelyabinsk-oblast.jpg';
-import ivanovo from '../images/mesto-russia-ivanovo.jpg';
-import kamchatka from '../images/mesto-russia-kamchatka.jpg';
-import kholmogorsky from '../images/mesto-russia-kholmogorsky-rayon.jpg';
-import baikal from '../images/mesto-russia-baikal.jpg';
-
-export const initialCards = [
-  {
-    name: 'Архыз',
-    link: arkhyz
-  },
-  {
-    name: 'Челябинская область',
-    link: chelyabinsk
-  },
-  {
-    name: 'Иваново',
-    link: ivanovo
-  },
-  {
-    name: 'Камчатка',
-    link: kamchatka
-  },
-  {
-    name: 'Холмогорский район',
-    link: kholmogorsky
-  },
-  {
-    name: 'Байкал',
-    link: baikal
-  }
-];
-
 export const settings = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -43,11 +9,14 @@ export const settings = {
 
 export const containerSelector = '.cards__list';
 export const cardSelector = '.card-template';
+
 export const popupWithImageSelector = '.popup-full-image';
 export const editPopupSelector = '.popup-profile-edit';
 export const addPopupSelector = '.popup-card-add';
+
 export const profileNameSelector = '.profile__name';
 export const profileAboutSelector = '.profile__about';
+
 export const nameInput = document.querySelector('.popup__input_edit_name');
 export const aboutInput = document.querySelector('.popup__input_edit_about');
 export const editButton = document.querySelector('.profile__edit-button');
